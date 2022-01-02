9f4809fc9495815862475fabe97eb0a962722beb
Isaac <jannealvillamin@gmail.com>
2022-01-02 20:58:31 +0800

    Fixed error 1, if guess is not first letter, 1st guess not saved

--- final_project.py
+++ final_project.py
@@ -1,6 +1,6 @@
 import random
 # Let us test our algorithm for Hangman
-nba = ["lebron james", "stephen curry", "kevin durant", "giannis antetokounmpo", "james harden", "anthony davis"]
+nba = ["lebron james", "stephen curry", "kevin durant", "giannis antetokounmpo", "james harden", "anthony davis", "damian lillard"]
 random_index = random.randint(0, len(nba) - 1)
 word = nba[random_index]
 temporary_string = ""
@@ -11,30 +11,35 @@ for letter in word:
         temporary_string += letter
 print(temporary_string)
 new_temporary_string = ""
-tries = 3
+lives = 3
 guess_word = ""
-while temporary_string != word and tries > 0 and guess_word != word:
+while temporary_string != word and lives > 0 and guess_word != word:
     guess = input("Guess letter: ")
     guess_word = input("Guess word (press ENTER or type any KEY if you don't have any guess): ")
     compare_string = temporary_string
+    lives_string = "Lives: {}".format(lives)
     if guess in word:
         for i in range(len(word)):
             if word[i] == guess:
                 new_temporary_string = compare_string[:i] + guess + compare_string[i+1:]
+            if new_temporary_string == "":
+                new_temporary_string = compare_string
             compare_string = new_temporary_string
+            print(compare_string)
         if temporary_string == new_temporary_string:
-            print("Oh no!")
+            print("Oh no! " + lives_string)
         elif temporary_string != new_temporary_string:
-            print(new_temporary_string)
-            print("Nice work!")
+            print(compare_string)
+            print("Nice work! " + lives_string)
     else:
         if new_temporary_string == "":
             print(temporary_string)
         else:
             print(new_temporary_string)
-        print("Oh no!")
-        tries = tries - 1         
-    temporary_string == new_temporary_string
+        lives = lives - 1
+        lives_string = "Lives: {}".format(lives)
+        print("Oh no! " + lives_string)         
+    temporary_string = new_temporary_string
 if temporary_string == word or guess_word == word:
     print("You win!")
 else:
